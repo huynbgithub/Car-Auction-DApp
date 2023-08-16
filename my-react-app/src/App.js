6f3a4183a2c74a8fced6926478b0855de2efa41b
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2023-08-16 20:59:39 +0700

    Home Page update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,13 +7,16 @@ import './lib/owlcarousel/assets/owl.carousel.min.css';
 import './lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css';
 import './css/style.css';
 import './css/bootstrap.min.css';
-import Home from "./components/index"
+import Home from "./components/Home"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
         <Route exact path='/'>
           <Home />
         </Route>
--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Home() {
 
     return (
-        <div>
+        <body>
             {/* Spinner Start */}
             {/* <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
                 <div className="spinner-border text-primary" style={{ width: '3rem', height: '3rem' }} role="status">
@@ -72,14 +72,14 @@ function Home() {
                 <div id="header-carousel" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <img className="w-100" src="" alt="Image" />
+                            <img className="w-100" src="img/carousel-bg-1.jpg" alt="Image" />
                             <div className="carousel-caption d-flex align-items-center">
                                 <div className="container">
                                     <div className="row align-items-center justify-content-center justify-content-lg-start">
                                         <div className="col-10 col-lg-7 text-center text-lg-start">
                                             <h6 className="text-white text-uppercase mb-3 animated slideInDown">// Car Servicing //</h6>
                                             <h1 className="display-3 text-white mb-4 pb-3 animated slideInDown">Qualified Car Repair Service Center</h1>
-                                            <a href className="btn btn-primary py-3 px-5 animated slideInDown">Learn More<i className="fa fa-arrow-right ms-3" /></a>
+                                            <a href="" className="btn btn-primary py-3 px-5 animated slideInDown">Learn More<i className="fa fa-arrow-right ms-3" /></a>
                                         </div>
                                         <div className="col-lg-5 d-none d-lg-flex animated zoomIn">
                                             <img className="img-fluid" src="img/carousel-1.png" alt />
@@ -89,7 +89,7 @@ function Home() {
                             </div>
                         </div>
                         <div className="carousel-item">
-                            <img className="w-100" src="" alt="Image" />
+                            <img className="w-100" src="img/carousel-bg-2.jpg" alt="Image" />
                             <div className="carousel-caption d-flex align-items-center">
                                 <div className="container">
                                     <div className="row align-items-center justify-content-center justify-content-lg-start">
@@ -577,7 +577,7 @@ function Home() {
             {/* Footer End */}
             {/* Back to Top */}
             <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top"><i className="bi bi-arrow-up" /></a>
-        </div>
+        </body>
 
     )
 }

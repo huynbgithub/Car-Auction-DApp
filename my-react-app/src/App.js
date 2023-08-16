0451b2584fe00101c342e76b7531910b5547f6c0
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2023-08-16 19:28:07 +0700

    template

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './lib/animate/animate.min.css';
+import './lib/owlcarousel/assets/owl.carousel.min.css';
+import './lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css';
+import './css/style.css';
+import './css/bootstrap.min.css';
+import Home from "./components/index"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

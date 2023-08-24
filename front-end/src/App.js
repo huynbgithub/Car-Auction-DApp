35f030dc72ea370d70a142d97868464113bbd20d
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2023-08-24 18:18:08 +0700

    Metamask Wallet and Update HomePage

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./components/Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;

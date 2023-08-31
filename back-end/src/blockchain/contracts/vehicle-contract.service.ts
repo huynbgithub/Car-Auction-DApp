93acc7b5d63859c7a4769feb4aa1cfc9b28a0d1a
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2023-08-31 17:52:17 +0700

    update formatter

--- back-end/src/blockchain/contracts/vehicle-contract.service.ts
+++ back-end/src/blockchain/contracts/vehicle-contract.service.ts
@@ -3,6 +3,7 @@ import { Web3Service } from '../web3.service'
 import vehicleContractAbi from '../../abi/vehicleContractAbi'
 import { Address } from 'web3'
 import { VehicleData } from '@defined-types/contract.type'
+import { formatVehicleData } from '@utils/formatter'
 
 @Injectable()
 export class VehicleContractService {
@@ -20,27 +21,7 @@ export class VehicleContractService {
     
     async getVehicleData(contractAddress: Address): Promise<VehicleData> {
         const result = await this.getVehicleContract(contractAddress).methods.getData().call()
-        return {
-            address: result.vehicleAddress.toString(),
-            deposit: result.deposit.toString(),
-            props: {
-                ownerFullName: result.props.ownerFullName.toString(),
-                ownerAddress: result.props.ownerAddress.toString(),
-                brand: result.props.brand.toString(),
-                vehicleType: result.props.vehicleType.toString(),
-                color: result.props.color.toString(),
-                seatCapacity: Number.parseInt(result.props.seatCapacity.toString()),
-                origin: result.props.origin.toString(),
-                licensePlate: result.props.licensePlate.toString(),
-                engineNumber: result.props.engineNumber.toString(),
-                chassisNumber: result.props.chassisNumber.toString(),
-                modelCode: result.props.modelCode.toString(),
-                capacity: Number.parseInt(result.props.capacity.toString()),
-                firstRegistrationDate: result.props.firstRegistrationDate.toString()
-            },
-            startingPrice: result.startingPrice.toString(),
-            vehicleImages: result.vehicleImages
-        }
+        return formatVehicleData(result)
     }
 
     // async getVehicleProperties(contractAddress: Address){
--- back-end/src/blockchain/contracts/vehicle-factory-contract.service.ts
+++ back-end/src/blockchain/contracts/vehicle-factory-contract.service.ts
@@ -3,6 +3,7 @@ import { Web3Service } from '../web3.service'
 import blockchainConfig from '@config/blockchain.config'
 import vehicleFactoryContractAbi from '../../abi/vehicleFactoryContractAbi'
 import { VehicleData } from '@defined-types/contract.type'
+import { formatVehicleData } from '@utils/formatter'
 
 @Injectable()
 export class VehicleFactoryContractService {
@@ -28,27 +29,7 @@ export class VehicleFactoryContractService {
 
         response.forEach(vehicleData => 
             results.push(
-                {   
-                    address: vehicleData.vehicleAddress.toString(),
-                    deposit: vehicleData.deposit.toString(),
-                    props: {
-                        ownerFullName: vehicleData.props.ownerFullName.toString(),
-                        ownerAddress: vehicleData.props.ownerAddress.toString(),
-                        brand: vehicleData.props.brand.toString(),
-                        vehicleType: vehicleData.props.vehicleType.toString(),
-                        color: vehicleData.props.color.toString(),
-                        seatCapacity: Number.parseInt(vehicleData.props.seatCapacity.toString()),
-                        origin: vehicleData.props.origin.toString(),
-                        licensePlate: vehicleData.props.licensePlate.toString(),
-                        engineNumber: vehicleData.props.engineNumber.toString(),
-                        chassisNumber: vehicleData.props.chassisNumber.toString(),
-                        modelCode: vehicleData.props.modelCode.toString(),
-                        capacity: Number.parseInt(vehicleData.props.capacity.toString()),
-                        firstRegistrationDate: vehicleData.props.firstRegistrationDate.toString()
-                    },
-                    startingPrice: vehicleData.startingPrice.toString(),
-                    vehicleImages: vehicleData.vehicleImages
-                }
+                formatVehicleData(vehicleData)
             )
         )
 

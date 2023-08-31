12491ed05653fa35931be6ba1ba9975c6f65d9b6
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2023-08-31 20:29:16 +0700

    okayyy

--- front-end/src/utils/contracts/VehicleFactoryContract.js
+++ front-end/src/utils/contracts/VehicleFactoryContract.js
@@ -3,8 +3,7 @@ import vehicleFactoryContractAbi from "../abi/VehicleFactoryContractAbi"
 import Web3 from "web3";
 
 export const getVehicleFactoryContract = (web3) => {
-    const eth = web3.eth;
-    return new eth.Contract(vehicleFactoryContractAbi, "0x8a23855d2f143697139dF3f9D6f2d5A4b5a12959");
+    return new web3.eth.Contract(vehicleFactoryContractAbi, "0x8a23855d2f143697139dF3f9D6f2d5A4b5a12959");
 }
 
 export const createVehicle = async (deposit, props, startingPrice, vehicleImages, web3) => {

36a1a142984db0b4b783401edd39f7b75954469e
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2023-09-08 16:40:13 +0700

    2nd-deployment-bug-fixed

--- front-end/src/utils/Constants.js
+++ front-end/src/utils/Constants.js
@@ -1,7 +1,7 @@
 import web3 from 'web3'
 
-export const exponent = Math.pow(10, 18)
-export const reciprocalExponent = 1 / Math.pow(10, 18)
+export const exponent = 1000000000000000000
+export const reciprocalExponent = 1 / 1000000000000000000
 
 export const gas = 30000000
 export const gasPrice = web3.utils.toWei(25, 'gwei')
